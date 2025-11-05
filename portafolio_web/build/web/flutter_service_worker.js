a806156a47af25a1d0faf481147569a2e8ba6ba8
YordiMon <yordiazaelmzmz@gmail.com>
2025-11-04 17:12:04 -0700

    Se creó la build necesaria para hostear el proyecto en la web

--- portafolio_web/build/web/flutter_service_worker.js
+++ portafolio_web/build/web/flutter_service_worker.js
@@ -33,7 +33,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "f4427b50378c8f0a8ef3ade54909fac3
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "e1899a5b3c11516a559a957713066110",
+"flutter_bootstrap.js": "9519306ee3d76e45daffed5ba6ac1932",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
